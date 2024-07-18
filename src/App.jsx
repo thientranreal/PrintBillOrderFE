9d864da08bf402371fcb3bcd263a2678be9740f4
thientranreal <thientt2612@gmail.com>
2024-07-18 08:50:14 +0700

    change jsx to js

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Dashboard from './components/Dashboard';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Dashboard from "./components/Dashboard";
 // import ProtectedRoute from './auth/ProtectedRoute';
 
-import history from './history';
+import history from "./history";
 function App() {
   return (
     <Router history={history}>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/dashboard"
-          element={
-            <Dashboard />
-          }
-        />
-
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <ToastContainer />
-
     </Router>
   );
-
 }
 
 export default App;
