e989b4afde3796be9ad0363417a2cee0f6ff2b80
thientranreal <thientt2612@gmail.com>
2024-07-25 11:16:39 +0700

    add bank account

--- src/components/BankAccount.jsx
+++ src/components/BankAccount.jsx
@@ -1,13 +1,110 @@
-import { Box, Divider, Paper, Typography } from "@mui/material";
+import {
+  Divider,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { StyledTableCell, StyledTableRow } from "./StyledComponents";
+import { useState } from "react";
+import TablePaginationActions from "./TablePaginationActions";
+
+function createData(id, bankName, accNum, owner) {
+  return { id, bankName, accNum, owner };
+}
+
+const rows = [
+  createData("1727", "Á Châu (ACB)", "6590677", "NGUYEN VAN LUAT"),
+  createData("1721", "Á Châu (ACB)", "6590677", "NGUYEN VAN LUAT"),
+  createData("1722", "Á Châu (ACB)", "6590677", "NGUYEN VAN LUAT"),
+  createData("1723", "Á Châu (ACB)", "6590677", "NGUYEN VAN LUAT"),
+  createData("1724", "Á Châu (ACB)", "6590677", "NGUYEN VAN LUAT"),
+];
 
 const BankAccount = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
     <Paper elevation={3} sx={{ p: 3 }}>
       <Typography variant="h5" gutterBottom>
         Tài khoản ngân hàng
       </Typography>
       <Divider />
-      <Box sx={{ height: 800, width: "100%" }}></Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>ID</StyledTableCell>
+              <StyledTableCell>Tên ngân hàng</StyledTableCell>
+              <StyledTableCell>Số tài khoản</StyledTableCell>
+              <StyledTableCell>Tên chủ tài khoản</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <StyledTableRow key={row.id}>
+                <StyledTableCell component="th" scope="row">
+                  {row.id}
+                </StyledTableCell>
+                <StyledTableCell>{row.bankName}</StyledTableCell>
+                <StyledTableCell>{row.accNum}</StyledTableCell>
+                <StyledTableCell>{row.owner}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+            {emptyRows > 0 && (
+              <TableRow sx={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={4}
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                slotProps={{
+                  select: {
+                    inputProps: {
+                      "aria-label": "rows per page",
+                    },
+                    native: true,
+                  },
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </Paper>
   );
 };
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,12 +1,49 @@
+import { Box, Button, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { Link } from "react-router-dom";
+import backgroundImage from "../assets/images/6325254.jpg";
+
+const BackgroundContainer = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  textAlign: "center",
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center center",
+  backgroundSize: "cover",
+  color: "#fff",
+  textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+}));
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+  padding: "10px 20px",
+  color: "#fff",
+  backgroundColor: "rgba(0, 123, 255, 0.7)",
+  textDecoration: "none",
+  borderRadius: "5px",
+  transition: "background-color 0.3s",
+  "&:hover": {
+    backgroundColor: "rgba(0, 123, 255, 1)",
+  },
+}));
+
 function NotFound() {
   return (
-    <div className="not-found-container">
-      <h1>404 - Page Not Found</h1>
-      <p>Oops! The page you are looking for does not exist.</p>
-      <a href="/login" className="back-to-home">
+    <BackgroundContainer>
+      <Typography variant="h1" sx={{ fontSize: 48, margin: "20px 0" }}>
+        404 - Page Not Found
+      </Typography>
+      <Typography variant="body1" sx={{ fontSize: 18, marginBottom: "30px" }}>
+        Oops! The page you are looking for does not exist.
+      </Typography>
+      <StyledButton component={Link} to="/login">
         Go back to the homepage
-      </a>
-    </div>
+      </StyledButton>
+    </BackgroundContainer>
   );
 }
 
